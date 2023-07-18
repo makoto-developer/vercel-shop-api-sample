c60d448034a4fb19c587f039c6ced08138675af0
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2023-07-19 00:41:30 +0900

    add shop list api

---
+++ interface/shop.ts
@@ -0,0 +1,9 @@
+export type Shop = {
+    id: string;
+    name: string;
+    url?: string;
+    email?: string;
+    address?: string;
+    phone?: string[];
+    openingHours: string;
+}
---
+++ pages/api/v1/shop/list.ts
@@ -0,0 +1,31 @@
+import {NextApiResponse, NextApiRequest} from 'next'
+import {Shop} from "@/interface/shop";
+
+export default function handler(
+    _req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const shopList: Shop[] = [
+        {
+            id: "1",
+            name: "東京営業所",
+            openingHours: "11:00-21:00",
+        }, {
+            id: "2",
+            name: "大阪営業所",
+            openingHours: "10:00-20:00",
+        }, {
+            id: "3",
+            name: "福岡営業所",
+            openingHours: "10:00-21:00",
+        }, {
+            id: "4",
+            name: "沖縄営業所",
+            openingHours: "9:30-17:00",
+        },
+    ]
+    return res
+        .status(200)
+        .appendHeader("message", "vercel_api")
+        .json(shopList)
+}
