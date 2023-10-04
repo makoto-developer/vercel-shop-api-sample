9ccc20b8b3c824617659c3de80156451ad368711
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2023-10-04 22:26:09 +0900

    update

--- pages/api/v1/shop/list.ts
+++ pages/api/v1/shop/list.ts
@@ -1,14 +1,12 @@
 import {NextApiResponse, NextApiRequest} from 'next'
-import {Shop} from "@/interface/shop";
 import {shop_list} from "@/pages/api/v1/shop/sample_data";
 
 export default function handler(
     _req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const shopList: Shop[] = shop_list
     return res
         .status(200)
         .appendHeader("message", "vercel_api")
-        .json(shopList)
+        .json(shop_list)
 }
